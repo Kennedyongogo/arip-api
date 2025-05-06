df0b9fa7f34f3c0cc0b67fb87ca8de986c09ad6e
mutetid <mutetidanz@gmail.com>
2025-05-06 16:34:38 +0300

    Remove unused authentication middleware import from userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-const { authenticateUser } = require("../middleware/authMiddleware");
 
 // Register new user
 router.post("/register", userController.register);
